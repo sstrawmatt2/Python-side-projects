3e9058201d733bfac6139fd7a66571d43e1fef5d
Sean S <sean.strawmatt@gmail.com>
2020-02-17 19:58:34 -0700

    Figuring out how to work Pycharm

--- Tester.py
+++ Tester.py
@@ -1,7 +1,24 @@
+################################################################################
+#                   DNA Sequence Tester                                        #
+#                                                                              #
+# PROGRAMMER:       Sean Strawmatt                                             #
+#                                                                              #
+# DESCRIPTION:                                                                 #
+# I'm trying to re-create the DNA sequence analysis from cs200(JAVA) in Python #
+#                                                                              #
+# COPYRIGHT:                                                                   #
+# This program is (c) 2018 Sean Strawmtt and Dean Zeller. This is original     #
+# work, without use of outside sources.                                        #
+################################################################################
 
-def start_function():
+def __init__(self):
+    self.dna = ""
+
+
+
+def basic_info(self):
     print("Hello")
-    dna_seq= str(input("please enter a DNA sequence => "))
-    print(dna_seq)
+    self.dna= str(input("please enter a DNA sequence => "))
+    print(self.dna)
 
 
